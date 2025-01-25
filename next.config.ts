5cc81ef4993dcdfa47faa0b81aab1e63d91d3fc4
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2025-01-25 09:06:48 -0500

    complete the home page next config for url

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+      }
+    ]
+  }
   /* config options here */
 };
 
